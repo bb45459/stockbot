8ff6c864092c65e3abf4b8ffbe94e101ef04590d
Bill Bloebaum <bill.bloebaum@dialexa.com>
2020-01-21 15:24:14 -0600

    add stock war image to bear

--- images.js
+++ images.js
@@ -6,9 +6,9 @@ module.exports = {
     "https://vignette.wikia.nocookie.net/vsbattles/images/b/bf/Kung-Fu-Panda-Render-copy.gif/revision/latest?cb=20151231071437",
     "https://files.chepicap.com/news/2018/09/10/v2_large_69a6963c4612753003283e0abac4a3d8d5030f8c.jpg",
     "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iJDLRm.3o2Iw/v0/1000x-1.jpg",
-    "https://i.imgur.com/aFx4JSJ.jpg",
     "https://i.ytimg.com/vi/iLtEcfqbBco/maxresdefault.jpg",
     "https://cdn.dribbble.com/users/415975/screenshots/1740996/attachments/281613/Mona_Lisa_Bear_dribBig.jpg",
+    "https://i.imgur.com/FAtw00Q.jpg"
   ],
   stonkey: 'https://i.redd.it/fk4fykhmay431.jpg',
   death: 'https://i.imgur.com/1pGnUqJ.jpg',
