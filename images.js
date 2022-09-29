64ac9b2c79834729485e95dee7aeb73928cd6333
BlabesJr <bill.bloebaum@dialexa.com>
2022-09-29 09:28:28 -0700

    Update images.js

--- images.js
+++ images.js
@@ -2,13 +2,12 @@ module.exports = {
   stonks: 'https://i0.kym-cdn.com/photos/images/original/001/262/983/2f0.png',
   wednesday: 'https://i.kym-cdn.com/photos/images/original/001/091/264/665.jpg',
   bears: [
-    "http://masonmaloofdesigns.com/wp-content/uploads/2012/09/TheGamblers.jpg",
+    "https://i.pinimg.com/originals/90/f7/20/90f72073b35e3ddbfbf6336abf35a225.jpg", // gambling bears
     "https://vignette.wikia.nocookie.net/vsbattles/images/b/bf/Kung-Fu-Panda-Render-copy.gif/revision/latest?cb=20151231071437",
-    "https://files.chepicap.com/news/2018/09/10/v2_large_69a6963c4612753003283e0abac4a3d8d5030f8c.jpg",
-    "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iJDLRm.3o2Iw/v0/1000x-1.jpg",
-    "https://i.ytimg.com/vi/iLtEcfqbBco/maxresdefault.jpg",
+    "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iJDLRm.3o2Iw/v0/1000x-1.jpg", // sad bear
+    "https://i.ytimg.com/vi/iLtEcfqbBco/maxresdefault.jpg", // xi pooh
     "https://cdn.dribbble.com/users/415975/screenshots/1740996/attachments/281613/Mona_Lisa_Bear_dribBig.jpg",
-    "https://i.imgur.com/FAtw00Q.jpg",
+    "https://i.imgur.com/FAtw00Q.jpg", // bear vs bull
     "https://makeavideo.studio/assets/A_confused_grizzly_bear_in_calculus_class.__21.webp"
   ],
   stonkey: 'https://i.redd.it/fk4fykhmay431.jpg',
