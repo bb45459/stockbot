b1dc7573267189d957b94d770014a611b5e65e2f
Christiano Rectenwaldino <cdrectenwald@gmail.com>
2024-01-02 17:40:52 -0600

    Add some pics b

--- images.js
+++ images.js
@@ -8,9 +8,11 @@ module.exports = {
     "https://i.ytimg.com/vi/iLtEcfqbBco/maxresdefault.jpg", // xi pooh
     "https://cdn.dribbble.com/users/415975/screenshots/1740996/attachments/281613/Mona_Lisa_Bear_dribBig.jpg",
     "https://i.imgur.com/FAtw00Q.jpg", // bear vs bull
-    "https://makeavideo.studio/assets/A_confused_grizzly_bear_in_calculus_class.__21.webp"
+    "https://makeavideo.studio/assets/A_confused_grizzly_bear_in_calculus_class.__21.webp",
+    "https://i.imgur.com/iNX3fpH.png" // pepe
   ],
   stonkey: 'https://i.redd.it/fk4fykhmay431.jpg',
   death: 'https://i.imgur.com/1pGnUqJ.jpg',
-  honks: 'https://i.redd.it/xqnszrre0xp31.jpg'
+  honks: 'https://i.redd.it/xqnszrre0xp31.jpg',
+  mama: 'https://i.imgur.com/mOeh0yZ.png'
 }
